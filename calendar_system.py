072304714c92f37bb26e165dfae9020e0743c6e5
Krabbenjack <chatheda@gmx.de>
2025-11-19 23:08:19 +0100

    Create new calendar system module

---
+++ calendar_system.py
@@ -0,0 +1,12 @@
+class CalendarSystem:
+    def __init__(self):
+        self.events = []
+
+    def add_event(self, event):
+        self.events.append(event)
+
+    def get_events(self):
+        return self.events
+
+    def clear_events(self):
+        self.events.clear()
