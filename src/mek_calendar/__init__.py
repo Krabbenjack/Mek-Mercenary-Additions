77f51bc674e0fc58258634598c46a4a9893ec581
Krabbenjack <chatheda@gmx.de>
2025-11-20 17:46:20 +0100

    Create/update src/mek_calendar/__init__.py

---
+++ src/mek_calendar/__init__.py
@@ -0,0 +1,15 @@
+from .calendar_system import (
+    MainCalendarWindow,
+    DetailedCalendarWindow,
+    EventManager,
+    Event,
+    RecurrenceType,
+)
+
+__all__ = [
+    "MainCalendarWindow",
+    "DetailedCalendarWindow",
+    "EventManager",
+    "Event",
+    "RecurrenceType",
+]
